0cee9b50ec2e0d94b3aa97752a63caaeb9a454e0
Viatcheslav Pestov <pestov74@gmail.com>
2023-03-15 20:00:17 +0500

    fix check_payment

--- billing_api/services/db.py
+++ billing_api/services/db.py
@@ -200,8 +200,8 @@ class DBService:
         res = await self.db.execute(select(Invoice).filter_by(id=payment.invoice_id))
         invoice: Invoice = res.scalars().first()
 
-        if invoice.start_date <= datetime.now() and (
-            datetime.now() < invoice.finish_date or invoice.finish_date is None
+        if invoice.start_date.replace(tzinfo=None) <= datetime.now() and (
+            invoice.finish_date is None or datetime.now() < invoice.finish_date.replace(tzinfo=None)
         ):
             return True
 
