a84cb5ecaa762eac306e51bc6e852061e77faac1
fall3n <aka.fall3n@gmail.com>
2023-03-19 16:50:16 +0300

    context_manager for db connection

--- billing_api/main.py
+++ billing_api/main.py
@@ -1,14 +1,21 @@
+from contextlib import asynccontextmanager
 from json import JSONEncoder
 from uuid import UUID
 
 import uvicorn
+from api.v1 import billing, content
+from core.config import settings
+from db.postgres import engine
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from fastapi.responses import ORJSONResponse
 from fastapi_pagination import add_pagination
 
-from api.v1 import billing, content
-from core.config import settings
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await engine.dispose()
 
 app = FastAPI(
     title=settings.project_name,
@@ -16,6 +23,7 @@ app = FastAPI(
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
     swagger_ui_parameters={"syntaxHighlight": False},
+    lifespan=lifespan,
 )
 
 add_pagination(app)
