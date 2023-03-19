34f2dc04eb97ffff011069ab9ad7d6dd9827e655
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-03-19 22:02:57 +0700

    fix 13

--- etl/etl.py
+++ etl/etl.py
@@ -4,7 +4,7 @@ import logging
 import psycopg2
 from psycopg2.extensions import connection as pg_connection
 from pydantic import ValidationError
-
+from psycopg2.extras import execute_batch
 from config import settings
 
 
@@ -56,7 +56,7 @@ class PostgresLoader:
     def save_all_data(self, db: str, inserted_data):
         # Получаем список имен полей для вставки
         fields_name_list = [key for key in inserted_data[0].dict()]
-        # Переформатирем список полей в строку в формат sql
+        # Переформатируется список полей в строку в формат sql
         fields_name_str = ", ".join([key for key in fields_name_list])
         fields_name_str_with_exclude = ", ".join([f"EXCLUDED.{key}" for key in fields_name_list])
         # Генерим строку по количеству элементов для вставки из %s для формата sql
@@ -66,11 +66,13 @@ class PostgresLoader:
         logging.debug(inserted_data_list)
         with self.connect.cursor() as cursor:
             try:
-                cursor.executemany(
+                execute_batch(
+                    cursor,
                     f"""INSERT INTO {db}({fields_name_str}) VALUES({amount_values_str}) 
                             ON CONFLICT (id) 
                             DO UPDATE SET ({fields_name_str}) = ({fields_name_str_with_exclude});""",
                     inserted_data_list,
+                    page_size=settings.fetch_limit,
                 )
             except (Exception, psycopg2.DatabaseError) as error:
                 logging.exception(error)
