b36f126065e638bcea0b798f4c8a5a6e01f7c497
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-03-19 19:13:50 +0700

    fix alembic connection to db

--- billing_api/init.sh
+++ billing_api/init.sh
@@ -4,6 +4,5 @@ set -exv
 chmod +x *.sh
 ls -l
 wait_for.sh "${POSTGRES__HOST}:5432"
-sed -i "s%sqlalchemy.url = postgresql://app:123qwe@127.0.0.1:5432/movies_database%sqlalchemy.url = postgresql://${POSTGRES__USER}:${POSTGRES__PASSWORD}@${POSTGRES__HOST}:5432/${POSTGRES__DB}%g" "alembic.ini"
 alembic upgrade head
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornH11Worker --bind 0.0.0.0:8000
